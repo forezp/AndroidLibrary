6889bd1f9dcc6b4b927e8cff461956e6d0de6923
forezp <124746406@qq.com>
2016-01-27 11:37:35 +0800

    Utils

--- mylibrary/src/main/java/com/trilink/androidlib/Constant.java
+++ mylibrary/src/main/java/com/trilink/androidlib/Constant.java
@@ -7,4 +7,9 @@ public class Constant {
 
     public  static final String SP_NAME="trlink";
     public  static final String  UPDATE_PATH="";
+    /**
+     * 引导页的
+     */
+    public static  final String GUIDE_NAME="guide_name";
+    public static final int GUIDE_VERSION = 1;
 }
